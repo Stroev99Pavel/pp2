a9e3ccbd1d2b3c2d1fb17b9acacc706432f0c2ed
Stroev99Pavel <156785262+Stroev99Pavel@users.noreply.github.com>
2024-01-21 22:13:03 +0600

    Add files via upload

---
+++ DataType.py
@@ -0,0 +1,29 @@
+#1)
+x = 5
+print(type(x))
+print("int")
+#2)
+x = "Hello World"
+print(type(x))
+print("str")
+
+#3)
+x = 20.5
+print(type(x))
+print("float")
+#4)
+x = ["apple", "banana", "cherry"]
+print(type(x))
+print("list")
+#5)
+x = ("apple", "banana", "cherry")
+print(type(x))
+print("tuple")
+#6)
+x = {"name": "John", "age": 36}
+print(type(x))
+print("dict")
+#7)
+x = True
+print(type(x))
+print("bool")
